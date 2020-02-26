db226e2be1d1da437368ee05e8994be79a744564
Bryn <thenazboz@gmail.com>
2020-02-26 09:29:26 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,29 +3,22 @@ class Song < ActiveRecord::Base
       belongs_to :genre
       has_many :notes
       
-      def artist_name=(name)
-        self.artist = Artist.find_or_create_by(name: name)
-      end
-    
-      def artist_name
-        self.artist ? self.artist.name : ''
-      end
-    
-      def genre_name=(name)
-        self.genre = Genre.find_or_create_by(name: name)
-      end
-    
-      def genre_name
-        self.genre ? self.genre.name : ''
-      end
-    
-      def note_contents
-        notes.collect {|note| note.content}
-      end
-    
-      def note_contents=(notes)
-        notes.each do |content|
-          note = self.notes.build(content: content) unless content.empty?
-        end
-      end
- end
+      
+  # This is some high-power Rails magic
+  # accepts_nested_attributes_for lets us create multiple Notes when creating a
+  # Song. That's pretty awesome. However we can define a proc (a small in-line
+  # method) that is passed the attributes that are about to be handed to the
+  # to-be-created Note and cancel the operation (reject_if) if they are judged
+  # to be invalid (i.e. the content attribute is empty). Soooo...powerful!
+  accepts_nested_attributes_for :notes, :reject_if => proc { |attrs| attrs[:content].blank? }
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    artist.try(:name)
+  end
+
+  ###learn?
+end
